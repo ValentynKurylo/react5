c8b22a2989d4c1ade85ecebdee1bee3b5e520072
ValentynKurylo <56786816+ValentynKurylo@users.noreply.github.com>
2021-05-17 14:20:19 +0300

    react5+page

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -3,24 +3,25 @@ import User from "./User";
 import UserDetails from "./UserDetails";
 export default function Users() {
   let [users, setUsers] = useState([])
-  let page = 1
-  let next = () => {
-    page++
+  let [page=1, setPage] = useState()
+  let page1 = () => {
+    setPage(page = 1)
+    //setPage(page)
     console.log(page);
   }
-  let before = () =>{
-    page--
+  let page2= () =>{
+    setPage(page=2)
     console.log(page);
   }
-useEffect(()=>{
+  useEffect(()=>{
 fetch('https://reqres.in/api/users?page='+ page).then(value => value.json()).then(value => {
   setUsers(value.data)
 })
-},[])
+},[page])
   return (
     <div>
       {users.map(value => <User item={value}/>)}
-      <button onClick={next}>Next</button> <button onClick={before}>Before</button>
+      <button onClick={page1}>page = 1</button> <button onClick={page2}>page = 2</button>
     </div>
   );
 }
